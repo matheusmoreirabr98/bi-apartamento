83149cab6590fd135cd579b9fe87137776fe80e9
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2026-03-06 23:37:12 -0300

    58

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import plotly.express as px
 import streamlit as st
 from supabase import create_client
 
-test = supabase.table("parcelas").select("*").execute()
-st.write("DEBUG parcelas:", test.data)
-
 # =========================================================
 # CONFIG
 # =========================================================
