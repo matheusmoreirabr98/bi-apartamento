f85910276083c409adbf4ed24cff6270dab5869e
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2026-03-13 01:41:40 -0300

    333

--- dashboard_todos.py
+++ dashboard_todos.py
@@ -781,7 +781,7 @@ def render_dashboard_todos(parcelas):
                 legend=dict(
                     orientation="h",
                     yanchor="top",
-                    y=-0.30,
+                    y=-0.20,
                     xanchor="center",
                     x=0.5,
                     traceorder="normal",
