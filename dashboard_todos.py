3460f283a82ce2bff36e14668a5fefb62f0595ae
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2026-03-13 01:48:19 -0300

    338

--- dashboard_todos.py
+++ dashboard_todos.py
@@ -785,6 +785,7 @@ def render_dashboard_todos(parcelas):
                     xanchor="center",
                     x=0.5,
                     traceorder="normal",
+                    font=dict(size=16),
                 ),
                 margin=dict(t=5, b=140, l=10, r=10),  # 👈 DIMINUIR ESSE T
             )
