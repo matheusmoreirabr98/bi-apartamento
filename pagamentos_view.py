06ff8667dae20ee20f98b0b6fb6bb7a1df2b9e6f
matheusmoreirabr98 <matheusmoreirabr98@gmail.com>
2026-03-14 01:52:04 -0300

    505

--- pagamentos_view.py
+++ pagamentos_view.py
@@ -791,7 +791,7 @@ def render_pagamentos_tab(parcelas_contrato, contrato_selecionado, supabase, pod
     with b1:
         _, centro_b1, _ = st.columns([1, 3, 1])
         with centro_b1:
-            st.markdown("<div style='height: 20px;'></div>", unsafe_allow_html=True)
+            st.markdown("<div style='height: 200px;'></div>", unsafe_allow_html=True)
             if st.button("Salvar Edição", type="primary", key="btn_salvar_edicao_pagamento", use_container_width=True):
                 try:
                     dados_atualizados = atualizar_pagamento_existente(
