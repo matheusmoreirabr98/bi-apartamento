3aa905c88e70bf631b6fc9495b63c87596ba0a8d
Matheus <matheusmoreirabr98@gmail.com>
2026-04-03 01:54:59 -0300

    553

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ div[data-testid="stButton"] > button[kind="secondary"]:hover {
 # =========================================================
 
 inject_styles()
-st.markdown("<h1 style='text-align: center;'>🏠 Apartamento teste</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>🏠 Apartamento</h1>", unsafe_allow_html=True)
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
